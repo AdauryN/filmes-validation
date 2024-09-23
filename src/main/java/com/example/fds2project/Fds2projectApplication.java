b363a72d6f465254850e33395eede93885ea2a67
TheoMoura <99195030+theomilll@users.noreply.github.com>
2024-09-23 11:05:06 -0300

    .

---
+++ src/main/java/com/example/fds2project/Fds2projectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fds2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fds2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fds2projectApplication.class, args);
+	}
+
+}
